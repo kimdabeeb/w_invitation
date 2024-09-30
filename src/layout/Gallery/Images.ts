594f858e718c95e835f3d6eefec871f76b603197
김다빈 <hobak0278@ypbooks.co.kr>
2024-09-30 13:56:57 +0900

    CLONE Frist Commit&Push

---
+++ src/layout/Gallery/Images.ts
@@ -0,0 +1,80 @@
+import image01 from '@/assets/images/img_1.webp'
+import image02 from '@/assets/images/img_2.webp'
+import image03 from '@/assets/images/img_3.webp'
+import image04 from '@/assets/images/img_4.webp'
+import image05 from '@/assets/images/img_5.webp'
+import image06 from '@/assets/images/img_6.webp'
+import image07 from '@/assets/images/img_7.webp'
+import image08 from '@/assets/images/img_8.webp'
+import image09 from '@/assets/images/img_9.webp'
+import image10 from '@/assets/images/img_10.webp'
+import image11 from '@/assets/images/img_11.webp'
+import image12 from '@/assets/images/img_12.webp'
+
+const images = [   /* 이미지 경로를 리스트로 저장 */
+  {
+    alt: 'image01',
+    source: image01,
+    width: 640,
+    height: 960
+  },
+  {
+    alt: 'image02',
+    source: image02,
+    width: 640,
+    height: 960
+  },
+  {
+    alt: 'image03',
+    source: image03,
+    width: 640,
+    height: 960
+  },{
+    alt: 'image04',
+    source: image04,
+    width: 640,
+    height: 960
+  },{
+    alt: 'image05',
+    source: image05,
+    width: 640,
+    height: 960
+  },{
+    alt: 'image06',
+    source: image06,
+    width: 640,
+    height: 960
+  },{
+    alt: 'image07',
+    source: image07,
+    width: 640,
+    height: 960
+  },{
+    alt: 'image08',
+    source: image08,
+    width: 640,
+    height: 960
+  },{
+    alt: 'image09',
+    source: image09,
+    width: 640,
+    height: 960
+  },{
+    alt: 'image10',
+    source: image10,
+    width: 640,
+    height: 960
+  },{
+    alt: 'image11',
+    source: image11,
+    width: 640,
+    height: 960
+  },{
+    alt: 'image12',
+    source: image12,
+    width: 640,
+    height: 960
+  }
+];
+
+export default images;
