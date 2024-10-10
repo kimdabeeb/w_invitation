180a7176b5d3df44bf679e3164e29398c3b43758
김다빈 <hobak0278@ypbooks.co.kr>
2024-10-10 10:28:21 +0900

    base url chaged commit & push

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,5 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/w_invitation/',
   plugins: [react(), tsconfigPaths(), svgr()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src/'),
-    },
-  },
 });
