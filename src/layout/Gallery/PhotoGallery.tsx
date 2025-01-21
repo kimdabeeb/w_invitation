27bdc8e7d00b0b063b18de3155b79cfbd2b83cde
on_db <kimdabeeb@github.com>
2025-01-21 17:25:28 +0900

    lazy loading err -7

--- src/layout/Gallery/PhotoGallery.tsx
+++ src/layout/Gallery/PhotoGallery.tsx
@@ -52,7 +52,7 @@ const PhotoGallery = () => {
     <Wrap style={{ paddingLeft: '0', paddingRight: '0'}}>
       <div data-aos="fade-up">
         <CustomEn>Gallery</CustomEn>
-        <TitleCont style={{ paddingBottom: 'calc(var(--el-between)* 2)'}}></TitleCont>
+        {/* <TitleCont style={{ paddingBottom: 'calc(var(--el-between)* 2)'}}></TitleCont> */}
         <TitleCont>
           <Title style={{fontSize:'.825rem'}}>사진을 클릭하시면 전체 화면 보기가 가능합니다</Title>
         </TitleCont>
